3b928f91c4f4ad3514abda4af40f19e70542ff91
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-06-22 22:45:30 +0300

    add changes to index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.querySelector(`#main`)
+main.remove()
+
+const newHeader = document.querySelector(`h1#victory`)
+newHeader.innerHTML = 
+"<h1 id='victory'></h1>"
